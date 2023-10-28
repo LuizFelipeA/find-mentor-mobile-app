c95025f2fdd158d3d296d53d983fbf38720ac7c3
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2023-10-28 13:04:20 -0400

    Responsibilities

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -8,7 +8,7 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from '../../hook/useFetch';
 
-const tabs = ["About", "Qualifications", "Responsabilities"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 
 const JobDetails = () => {
@@ -36,8 +36,11 @@ const JobDetails = () => {
                 return <JobAbout 
                     info={data[0].job_description ?? "No data provided"}
                 />
-            case "Responsabilities":
-        
+            case "Responsibilities":
+                return <Specifics
+                    title="Responsibilities"
+                    points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                />
             default:
                 break;
         }
